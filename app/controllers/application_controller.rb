90ff2d57dc91941874502d2fdf4c73e747dbdebc
Peter Prabu <peter.rajendraprabus@manpowergroup.com>
2017-12-19 21:30:00 +0530

    testtt

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  request.env['http://www.manpower.com']
+
   private
   
   #-> Prelang (user_login:devise)
